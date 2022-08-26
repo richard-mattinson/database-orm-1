746552c59aca9e1361a93c4374b3783037d3007e
Richard Mattinson <richard.m@ttinson.net>
2022-08-26 14:51:20 +0100

    ex1 complete

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,14 +5,22 @@ async function seed() {
     const createdCustomer = await prisma.customer.create({
         data: {
             name: 'Alice'
-        }
+        },
+        include: {contact: true}
     });
 
     console.log('Customer created', createdCustomer);
 
     // Add your code here
 
+    const createdContact = await prisma.contact.create({
+        data: {
+            email: 'des@gds.bant',
+            phone: '1800-wonk'
+        }
+    });
 
+    console.log('Contact created', createdContact);
 
 
     // Don't edit any of the code below this line
